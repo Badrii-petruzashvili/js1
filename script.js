1c6c3ae944773d1a742b9948bfac34058fa04bc1
Badrii-petruzashvili <126329141+Badrii-petruzashvili@users.noreply.github.com>
2023-04-28 21:22:20 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+//first
+// let number = 100;
+// if ( number < 50){
+//     console.log('Less than 50');
+// }
+// else if ( number > 20){
+//     console.log('More than 20');
+// }
+// else {
+//     console.log('Error');
+// }
+
+
+
+//second
+
+// let userName = "Mariam";
+
+// 1
+// let result = ('userName =', !"Mariam") ? 'True' : 'Error';
+// console.log(result);
+
+
+// 2
+// if (userName == "Mariam") {
+//     console.log("True");
+// }
+// else {
+//     console.log("Error");
+// }
+
+
+
+//third
+
+// switch (userName) {
+//     case "Mariam":
+//         console.log('True');
+//         break;
+//     default:
+//         console.log('Error');
+// }
+
+
+
